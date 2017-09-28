76727a33307ef6085954db06598155a14bcf3dac
ahmad-mukhlish <ahmad_mukhlish_s@yahoo.co.id>
2017-09-28 15:22:52 +0700

    Add date

--- src/main/MHSupplier.java
+++ src/main/MHSupplier.java
@@ -5,6 +5,8 @@
  */
 package main;
 
+import frames.MainFrame ;
+
 /**
  *
  * @author GOODWARE1
@@ -15,7 +17,8 @@ public class MHSupplier {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+           MainFrame app = new MainFrame() ;
+           app.setVisible(true);
     }
     
 }
