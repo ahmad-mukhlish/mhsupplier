c92b163ae5defe33f7aaf56aa1e8ea6ae8af305b
ahmad-mukhlish <ahmad_mukhlish_s@yahoo.co.id>
2017-09-28 14:26:10 +0700

    Add resources

--- src/main/MHSupplier.java
+++ src/main/MHSupplier.java
@@ -3,13 +3,13 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package mhsupplier;
+package main;
 
 /**
  *
  * @author GOODWARE1
  */
-public class Main {
+public class MHSupplier {
 
     /**
      * @param args the command line arguments
